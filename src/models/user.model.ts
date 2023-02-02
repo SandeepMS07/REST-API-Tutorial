ef5a7fcca7d9853604d0861aa4c561482f3c217b
Sandeep M S <sandeepms746@gmail.com>
2023-02-02 09:50:20 +0530

    added user.model code

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -8,6 +8,12 @@ export interface UserInput {
   password: string;
 }
 
+export interface UserDocument extends UserInput, mongoose.Document {
+  createdAt: Date;
+  updatedAt: Date;
+  comparePassword(candidatePassword: string): Promise<boolean>;
+}
+
 const userSchema = new mongoose.Schema(
   {
     email: {
@@ -28,6 +34,29 @@ const userSchema = new mongoose.Schema(
     timestamps: true,
   }
 );
+
+userSchema.pre("save", async function (next) {
+  let user = this as UserDocument;
+
+  if (!user.isModified("password")) {
+    return next();
+  }
+
+  const salt = await bcrypt.genSalt(config.get<number>("saltWorkFactor"));
+  const hash = await bcrypt.hashSync(user.password, salt);
+
+  user.password = hash;
+
+  return next();
+});
+
+userSchema.methods.comparePassword = async function (
+  candidatePassword: string
+): Promise<boolean> {
+  const user = this as UserDocument;
+
+  return bcrypt.compare(candidatePassword, user.password).catch((e) => false);
+};
 
 const UserModel = mongoose.model("User", userSchema);
 export default UserModel;
